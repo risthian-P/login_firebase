3216547acbce156d31db7b287eae34baef780b47
cristhian <cristhian.panora@gmail.com>
2024-06-09 21:02:23 -0500

    cambios en singup, tsconfig

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,13 +10,16 @@ import { AppRoutingModule } from './app-routing.module';
 import {AngularFireModule} from '@angular/fire/compat';
 import { AngularFireAuthModule} from '@angular/fire/compat/auth'
 import { environment } from 'src/environments/environment';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, AngularFireModule, AngularFireAuthModule,
     AngularFireModule.initializeApp(environment.firebaseConfig)
   ],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, provideFirebaseApp(() => initializeApp({"projectId":"loginfirebase-8b725","appId":"1:731101396586:web:2d1f61146eb444b4c05413","storageBucket":"loginfirebase-8b725.appspot.com","apiKey":"AIzaSyAka8UtjxsKUNSvStbQeaF35Mofa0Ky2Y8","authDomain":"loginfirebase-8b725.firebaseapp.com","messagingSenderId":"731101396586"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/pages/singup/singup.page.ts
+++ src/app/pages/singup/singup.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { LoadingController } from '@ionic/angular';
+import { AutheticationService } from 'src/app/authetication.service';
 
 @Component({
   selector: 'app-singup',
@@ -7,10 +9,36 @@ import { FormGroup } from '@angular/forms';
   styleUrls: ['./singup.page.scss'],
 })
 export class SingupPage implements OnInit {
-  regForm: FormGroup | undefined;
-  constructor() { }
+
+  regForm : FormGroup
+
+
+    constructor(public formBuilder: FormBuilder, public loadingCtrl: LoadingController, public authService:AutheticationService) { }
 
   ngOnInit() {
+    this.regForm = this.formBuilder.group({
+      fullname :['', [Validators.required]],
+      email :['', [
+        Validators.required,
+        Validators.email,
+        Validators.pattern("[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$")
+      ]],
+      password :['', [
+        Validators.required,
+        Validators.pattern("(?=.*\d)(?=.*[a-z])(?=.*[0-9])(?=.*[A-Z]).{8,}")
+      ]]
+      
+    })
+  }
+  get errorControl(){
+    return this.regForm?.controls;
+  }
+  async singUp(){
+    const loading = await this.loadingCtrl.create();
+    await loading.present();
+    if(this.regForm?.valid){
+      // const user = await this.authService.registerUser(email,password)
+    }
   }
 
 }
